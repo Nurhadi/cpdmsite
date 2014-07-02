8c26183307e52c6a2d382f6e19f89aec12f76bf3
Nurhadi <nurhadimaulana92@yahoo.com>
2014-07-02 07:02:03 +0700

    Update - Main Menu

--- application/views/main_menu_view.php
+++ application/views/main_menu_view.php
@@ -14,8 +14,54 @@
 			<!-- Collect the nav links, forms, and other content for toggling -->
 			<div class="collapse navbar-collapse navbar-ex1-collapse">
 				<ul class="nav navbar-nav">
-					<li><a href="<?php echo site_url('peserta/daftar'); ?>">Daftar Pelatihan</a></li>
-					<li><a href="<?php echo site_url('peserta/upload_bukti_pembayaran'); ?>">Upload Bukti Pembayaran</a></li>
+	        <li class="dropdown">
+	          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Profile</span></a>
+	          <ul class="dropdown-menu" role="menu">
+	            <li><a href="#">Latar Belakang</a></li>
+	            <li><a href="#">Visi dan Misi</a></li>
+	            <li><a href="#">Pengelola</a></li>
+	          </ul>
+	        </li>
+	        <li class="dropdown">
+	          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Pelatihan</span></a>
+	          <ul class="dropdown-menu" role="menu">
+				      <li class="dropdown-submenu"> <a tabindex="-1" href="#">Bimtek</a>
+	              <ul class="dropdown-menu">
+                  <li><a tabindex="-1" href="#">Penjelasan Program Bimtek</a></li>
+                  <li><a href="#">Kurikulum dan Struktur Bidang</a></li>
+                  <li><a href="#">Jadwal</a></li>
+                  <li><a href="#">Fasilitas</a></li>
+                  <li class="dropdown-submenu"><a href="#">Peserta</a>
+                  	<ul class="dropdown-menu">
+                  		<li><a href="#">Persyaratan Peserta</a>
+                  		<li><a href="#">Peserta Terdaftar</a>
+                  	</ul>
+                  </li>
+                  <li><a href="#">Evaluasi</a></li>
+                  <li><a href="#">Sertifikat</a></li>
+                  <li><a href="#">Informasi Penginapan</a></li>
+                </ul>
+               </li>
+	            <li><a href="#">Pengelola Laboratorium Universitas / Perguruan Tinggi</a></li>
+	            <li><a href="#">HOT - Based Science and Mathematics Pedagogy</a></li>
+	            <li><a href="#">E-Learning in SMITE</a></li>
+	            <li><a href="#">Preparation on SMITE Olympiade</a></li>
+	          </ul>
+	        </li>
+					<!-- <li><a href="<?php echo site_url('peserta/daftar'); ?>">Pendaftaran</a></li> -->
+					<li><a href="#">Download</a></li>
+					<li><a href="#">Pilot School</a></li>
+					<li><a href="#">Galeri</a></li>
+					<!-- <li><a href="<?php echo site_url('peserta/upload_bukti_pembayaran'); ?>">Upload Bukti Pembayaran</a></li> -->
+				</ul>
+
+				<ul class="nav navbar-nav navbar-right">
+	        <li>
+	        	<a href="<?php echo site_url('peserta/daftar'); ?>">
+	        		<!-- <button class="btn btn-success" style="background:transparent; color:#01C1F4; border:1px solid #01C1F4;">Daftar</button> -->
+	 						<button style="border:1px solid #01C1F4; background:transparent; color:#01C1F4; border-radius:5px; padding:0 35px; font-size:0.8em;">Daftar</button>
+	        	</a>
+	        </li>
 				</ul>
 			</div><!-- /.navbar-collapse -->
 			</div>
