be199a121932d71c8e482dcc1d4e52bce9925c6a
Nurhadi <nurhadimaulana92@yahoo.com>
2014-07-08 06:44:50 +0700

    Update - Check login status in login page

--- onix/application/controllers/login.php
+++ onix/application/controllers/login.php
@@ -4,21 +4,26 @@ class Login extends CI_Controller {
 
 	public function __construct()
 	{
-		parent::__construct();		
+		parent::__construct();
 		$this->load->model('admin_model');
 		// $this->output->enable_profiler(true);
 	}
 
 	public function index()
 	{
-		$this->load->view('login_view');
+		if($this->session->userdata('email') !== false && $this->session->userdata('logged_in') !== false){
+			redirect('homepage');
+		}
+		else{
+			$this->load->view('login_view');
+		}
 	}
 
 	public function login_process()
 	{
 		$email = $this->input->post('email');
 		$password = $this->input->post('password');
-		
+
 		$admin_auth = $this->admin_model->check_admin_authentication($email, $password);
 		if($admin_auth->num_rows() > 0)
 		{
@@ -40,7 +45,7 @@ class Login extends CI_Controller {
 	{
 		$this->session->unset_userdata('email');
 		$this->session->unset_userdata('logged_in');
-		
+
 		redirect('/login');
 	}
 }
